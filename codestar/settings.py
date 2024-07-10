0aac6b604ee2deba022887d22a511d661df43ad5
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-07-10 07:29:17 +0000

    add approved comments to posts

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com',
  '8000-serrakd-djangoblog-t6or9xb99nm.ws.codeinstitute-ide.net',
